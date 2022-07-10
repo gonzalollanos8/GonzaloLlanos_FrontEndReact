c07465d81a5463806656747ba65a9d203001012b
Gonzalo Llanos <emanuelgonzalo98@gmail.com>
2022-07-10 15:17:29 -0300

    primer commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Nav from "./Nav";
+
+const App =()=> 
+<>
+<h1>hola mundo</h1>
+<Nav/>
+</>
+
+export default App
---
+++ src/Nav.js
@@ -0,0 +1,12 @@
+
+
+const Nav =()=>{
+<>
+    <button>inicio</button>
+    <button>Mis albumes</button>
+    <button>buscar</button>
+</>
+
+}
+
+export default Nav
